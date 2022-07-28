91eeeb92ecd44576c6a565511fa68a76ca267e7a
Matdrox <Matdrox@users.noreply.github.com>
2022-07-28 16:35:30 +0200

    Added 'Welcome' Text

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div className='bg-slate-200 h-screen'>
-      <p className='text-orange-500'>HELLO</p>
+    <div className='bg-orange-100 h-screen'>
       <Spline scene="https://prod.spline.design/0j-He-n7S2unukca/scene.splinecode" />
     </div>
   );
