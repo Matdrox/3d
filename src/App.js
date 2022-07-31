ac1a9bff6d7d37598aced0958ca12d7d6523f5eb
Matei Cananau <mateicananau@gmail.com>
2022-07-31 22:50:22 +0200

    Updated Comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Spline from '@splinetool/react-spline';
 
 function App() {
-  return (
-    <div className='bg-orange-100 h-screen'>
-      <Spline scene="https://prod.spline.design/0j-He-n7S2unukca/scene.splinecode" />
-    </div>
-  );
+	return (
+		<div className='bg-orange-100 h-screen'>
+			<Spline scene='https://prod.spline.design/0j-He-n7S2unukca/scene.splinecode' />
+			{/* Show Scroll Down */}
+		</div>
+	);
 }
 
 export default App;
